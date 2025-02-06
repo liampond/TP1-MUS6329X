af01e526e7dbdbeeb43f8df6654186e33a2ca4bc
Liam Pond <104945840+liampond@users.noreply.github.com>
2025-02-05 21:09:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+location = "45.509379%2C%20-73.608654"
+api_key = os.gentenv("api_key")
+
+url = f"https://api.tomorrow.io/v4/weather/realtime?location={location}&apikey={api_key}"
+
+headers = {
+    "accept": "application/json",
+    "accept-encoding": "deflate, gzip, br"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
